bdfdb9acfdf1a5d13e8f4c23f6949755e8e4f25e
Nikelroot <nikel.root@gmail.com>
2026-01-20 14:05:08 +0500

    1

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
-import type { NextConfig } from 'next'
-import { PHASE_DEVELOPMENT_SERVER } from 'next/dist/shared/lib/constants'
+import type { NextConfig } from "next";
+import { PHASE_DEVELOPMENT_SERVER } from "next/dist/shared/lib/constants";
 
 const nextConfig = (phase: string): NextConfig => {
-  const isDev = phase === PHASE_DEVELOPMENT_SERVER
+  const isDev = phase === PHASE_DEVELOPMENT_SERVER;
   return {
     compiler: {
       styledComponents: true,
@@ -11,14 +11,14 @@ const nextConfig = (phase: string): NextConfig => {
       // },
     },
     async rewrites() {
-      if (!isDev) return []
+      if (!isDev) return [];
       return [
         {
-          source: '/api/:path*',
-          destination: 'http://localhost:3001/:path*',
+          source: "/api/:path*",
+          destination: "http://localhost:3001/:path*",
         },
-      ]
+      ];
     },
-  }
-}
-export default nextConfig
+  };
+};
+export default nextConfig;
