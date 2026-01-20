00f00ebe786e1b69c587d6ed2862e99951093ed0
Nikelroot <nikel.root@gmail.com>
2026-01-20 14:08:33 +0500

    1

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
-import type { NextConfig } from "next";
-import { PHASE_DEVELOPMENT_SERVER } from "next/dist/shared/lib/constants";
+import type { NextConfig } from 'next'
+import { PHASE_DEVELOPMENT_SERVER } from 'next/dist/shared/lib/constants'
 
 const nextConfig = (phase: string): NextConfig => {
-  const isDev = phase === PHASE_DEVELOPMENT_SERVER;
+  const isDev = phase === PHASE_DEVELOPMENT_SERVER
   return {
     compiler: {
       styledComponents: true,
@@ -10,9 +10,9 @@ const nextConfig = (phase: string): NextConfig => {
       // exclude: ['error'],
       // },
     },
-    turbopack: {
-      root: __dirname,
-    },
+    // turbopack: {
+    //   root: __dirname,
+    // },
     reactStrictMode: false,
     env: {
       PUBLIC_API_URL: 'https://abook.nikelroot.ru',
@@ -21,14 +21,14 @@ const nextConfig = (phase: string): NextConfig => {
     output: 'standalone',
     productionBrowserSourceMaps: false,
     async rewrites() {
-      if (!isDev) return [];
+      if (!isDev) return []
       return [
         {
-          source: "/api/:path*",
-          destination: "http://localhost:3001/:path*",
+          source: '/api/:path*',
+          destination: 'http://localhost:3001/:path*',
         },
-      ];
+      ]
     },
-  };
-};
-export default nextConfig;
+  }
+}
+export default nextConfig
